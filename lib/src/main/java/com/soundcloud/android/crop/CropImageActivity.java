6a86d35823ba8c5ce27ad57082ff244d951cff65
danwilkie <danwilkie777@gmail.com>
2015-09-22 11:21:11 +0100

    Increase initial crop area to 100% instead of 4/5

--- lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
+++ lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
@@ -223,8 +223,8 @@ public class CropImageActivity extends MonitoredActivity {
 
             Rect imageRect = new Rect(0, 0, width, height);
 
-            // Make the default size about 4/5 of the width or height
-            int cropWidth = Math.min(width, height) * 4 / 5;
+            // Make the default size about 100% of the width or height
+            int cropWidth = Math.min(width, height);
             @SuppressWarnings("SuspiciousNameCombination")
             int cropHeight = cropWidth;
 
